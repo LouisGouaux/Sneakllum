c96f4ff0af82931bd5a25e5ae1fe44c66b880c20
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2024-11-08 11:47:12 +0100

    Refactor [API]: added sku column in product model

--- api/database/migrations/2024_11_08_094111_create_products_table.php
+++ api/database/migrations/2024_11_08_094111_create_products_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("sku")->unique();
             $table->string("brand");
             $table->string("name");
             $table->string("color");

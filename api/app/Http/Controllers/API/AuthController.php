33abda53309096b1b25b452459ffc42312da25fd
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2024-11-08 15:40:20 +0100

    Feat [API]: Created Register end point

---
+++ api/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        $data = $request->validate([
+            'first_name' => ['required', 'string', 'max:50'],
+            'last_name' => ['required', 'string', 'max:50'],
+            'email' => ['required', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
+        ]);
+        $user = User::create($data);
+        $token = $user->CreateToken('auth_token')->plainTextToken;
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'token' => $token
+            ],
+            'message' => 'User created successfully'
+        ]);
+    }
+}

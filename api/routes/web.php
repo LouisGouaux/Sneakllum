cdbc2a7af9ce09e600a870d1d871346440654cbc
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2024-11-08 12:12:00 +0100

    Feat [API]: Added store product method

--- api/routes/web.php
+++ api/routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get("store_product", [\App\Http\Controllers\Batch\add_product::class, "store_sneaker"]);

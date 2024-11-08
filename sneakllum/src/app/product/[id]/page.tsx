4ffc5088997d85e157ba0b4ec3c1be419d80ae66
robinivan <ivan.robin@epitech.digital>
2024-11-08 11:22:56 +0100

    Finalise navigation

--- sneakllum/src/app/product/[id]/page.tsx
+++ sneakllum/src/app/product/[id]/page.tsx
@@ -1,12 +1,12 @@
-import { useRouter } from 'next/navigation';
+'use client';
+import { useParams } from 'next/navigation'
 
 export default function ProductPage() {
-    const router = useRouter();
-    const { id } = router.query; // This retrieves the product ID from the route
-
+    const params = useParams<{ id: number }>()
+    console.log(params)
     return (
         <div>
-            <h2>Product Details - {id}</h2>
+            <h2>Product Details - {params.id}</h2>
             {/* Display product details based on the ID */}
         </div>
     );

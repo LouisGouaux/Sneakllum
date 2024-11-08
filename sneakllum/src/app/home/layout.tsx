67485512b621267170cffb4184ec1370a971a5c8
robinivan <ivan.robin@epitech.digital>
2024-11-08 10:53:14 +0100

    bug verion home page

---
+++ sneakllum/src/app/home/layout.tsx
@@ -0,0 +1,16 @@
+import "../globals.css";
+import React from "react";
+
+
+export default function RootLayout({
+
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+      </body>
+    </html>
+  );
+}

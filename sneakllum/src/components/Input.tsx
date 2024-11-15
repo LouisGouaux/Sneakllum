c24ac842a25613b54ec66a058de2672952d2ddde
robinivan <ivan.robin@epitech.digital>
2024-11-15 10:56:07 +0100

    Update Input.tsx

--- sneakllum/src/components/Input.tsx
+++ sneakllum/src/components/Input.tsx
@@ -9,7 +9,7 @@ interface InputProps {
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Input({ type = "text", name, placeholder, value, onChange }: InputProps) {
+export default function Input({ type = "text", name, placeholder, value, onChange, className }: InputProps) {
     return (
         <input
             type={type}
@@ -17,7 +17,7 @@ export default function Input({ type = "text", name, placeholder, value, onChang
             placeholder={placeholder}
             value={value}
             onChange={onChange}
-            className="w-full border border-gray-300 rounded px-4 py-2 focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+            className={`w-full border border-gray-300 rounded px-4 py-2 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 ${className}`}
         />
     );
 }

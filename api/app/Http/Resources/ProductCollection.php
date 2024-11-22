13453c183d73a09aba9e75bea03b209403226b53
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2024-11-22 11:57:36 +0100

    Feat [API]: Created Product Collection

---
+++ api/app/Http/Resources/ProductCollection.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => $this->collection->map(function ($product) {
+                return [
+                    'id' => $product->id,
+                    'brand' => $product->brand,
+                    'name' => $product->name,
+                    'price' => $product->price,
+                    'image' => $product->image,
+                ];
+            }),
+            'links' => [
+                'first' => $this->resource->url(1),
+                'last' => $this->resource->url($this->resource->lastPage()),
+                'prev' => $this->resource->previousPageUrl(),
+                'next' => $this->resource->nextPageUrl(),
+            ],
+            'meta' => [
+                'current_page' => $this->resource->currentPage(),
+                'from' => $this->resource->firstItem(),
+                'last_page' => $this->resource->lastPage(),
+                'path' => $this->resource->path(),
+                'per_page' => $this->resource->perPage(),
+                'to' => $this->resource->lastItem(),
+                'total' => $this->resource->total(),
+            ],
+        ];
+    }
+}

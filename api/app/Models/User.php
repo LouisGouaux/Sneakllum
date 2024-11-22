3d7610a84db6ce336c3a1571e55ec7df3f793c5e
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2024-11-22 15:48:45 +0100

    Feat [API]: Created relationship between user and basket

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function basket()
+    {
+        return $this->hasOne(Basket::class);
+    }
 }

4a5aed5430590893e6908130fdaff04a86fbc5ee
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2024-11-22 15:04:02 +0100

    feat [API]: Created check-stock endpoint

--- api/app/Http/Controllers/API/ProductController.php
+++ api/app/Http/Controllers/API/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductCollection;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use App\Models\Variant;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -24,6 +25,17 @@ class ProductController extends Controller
         $products = Product::orderBy('release_date', 'desc')->paginate(20);
         return new ProductCollection($products);
     }
+
+    public function check_product_stock($id, Request$request)
+    {
+        $product_variant = Variant::where('product_id', $id)->where('size_id', $request->size_id)->where('color_id', $request->color_id)->first();
+        return response()->json([
+            'is_available' => $product_variant->stock > 0,
+            'stock' => $product_variant->stock,
+            'variant_id' => $product_variant->id
+        ]);
+    }
+
     /**
      * Store a newly created resource in storage.
      */
--- api/routes/api.php
+++ api/routes/api.php
@@ -10,4 +10,5 @@ Route::post('register', [\App\Http\Controllers\API\AuthController::class, 'regis
 Route::post('login', [\App\Http\Controllers\API\AuthController::class, 'login']);
 Route::get('products/new', [\App\Http\Controllers\API\ProductController::class, 'new_product']);
 Route::get('products/{id}', [\App\Http\Controllers\API\ProductController::class, 'show']);
+Route::get('products/{id}/check', [\App\Http\Controllers\API\ProductController::class, 'check_product_stock']);
 Route::get('products', [\App\Http\Controllers\API\ProductController::class, 'index']);

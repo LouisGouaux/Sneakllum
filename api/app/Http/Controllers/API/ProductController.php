66525c2f27055e05ccaa459d3de5cc19c7ec7641
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2024-11-29 11:29:59 +0100

    Feat [API]: Added size filter

--- api/app/Http/Controllers/API/ProductController.php
+++ api/app/Http/Controllers/API/ProductController.php
@@ -19,7 +19,8 @@ class ProductController extends Controller
         $request->validate([
             'sort' => ['string', 'max:5'],
             'brand' => ['string'],
-            'gender' => ['string', 'max:10']
+            'gender' => ['string', 'max:10'],
+            'size' => ['integer', 'min:15', 'max:49']
         ]);
         if ($request->sort === 'new') {
             $products = $this->new_product();
@@ -31,18 +32,27 @@ class ProductController extends Controller
             if ($request->has('gender')) {
                 $query->where('gender', $request->gender);
             }
+            if ($request->has('size')) {
+                $size = $request->size;
+                $query->whereHas('sizes', function ($q) use ($size) {
+                    $q->where('size', $size);
+                });
+            }
+
             $products = $query->paginate(20);
         }
         return new ProductCollection($products);
     }
 
-    private function new_product()
+    private
+    function new_product()
     {
         $products = Product::orderBy('release_date', 'desc')->paginate(20);
         return $products;
     }
 
-    public function check_product_stock($id, Request $request)
+    public
+    function check_product_stock($id, Request $request)
     {
         $product_variant = Variant::where('product_id', $id)->where('size_id', $request->size_id)->where('color_id', $request->color_id)->first();
         return response()->json([
@@ -55,7 +65,8 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public
+    function store(Request $request)
     {
         //
     }
@@ -63,7 +74,8 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show($id)
+    public
+    function show($id)
     {
         $product = Product::find($id);
         return response()->json(new ProductResource($product));
@@ -72,7 +84,8 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Product $product)
+    public
+    function update(Request $request, Product $product)
     {
         //
     }
@@ -80,7 +93,8 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public
+    function destroy(Product $product)
     {
         //
     }

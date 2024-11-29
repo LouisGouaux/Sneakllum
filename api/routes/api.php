04e17101e989a4e1c8d07d083e753778d4478d21
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2024-11-29 11:45:25 +0100

    Feat [API]: CCreated logout endpoint

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [\App\Http\Controllers\API\AuthController::class, 'logout']);
     Route::post('basket', [\App\Http\Controllers\API\BasketController::class, 'store']);
 });
 

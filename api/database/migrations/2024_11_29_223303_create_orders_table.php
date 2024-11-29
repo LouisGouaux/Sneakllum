3b53a2c3f94a484269cc9f27bedd6d265aed5c4d
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2024-11-30 00:18:26 +0100

    Feat [API]: Created Order model and associated migration

---
+++ api/database/migrations/2024_11_29_223303_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_number', 6)->unique();
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->string('user_first_name');
+            $table->string('user_last_name');
+            $table->string('user_email');
+            $table->string('user_phone');
+            $table->bitInteger('total_amount');
+            $table->longText('shipping_address');
+            $table->longText('billing_address');
+            $table->string('status')->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};

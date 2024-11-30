4b1422b20a8a6d7125f86c52a694b7c6793f3f65
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2024-11-30 22:00:38 +0100

    Feat [API]: Checked database assertions in BasketTest

--- api/tests/Feature/BasketTest.php
+++ api/tests/Feature/BasketTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\Basket;
 use App\Models\User;
+use App\Models\Variant;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
@@ -11,20 +13,11 @@ class BasketTest extends TestCase
     /**
      * A basic feature test example.
      */
-
-protected $user;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->user = User::factory()->create();
-    }
-
-
     public function test_user_can_create_a_basket(): void
     {
-        $response = $this->actingAs($this->user)->postJson('api/basket', [
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->postJson('api/basket', [
             [
                 'product_id' => 1,
                 'size_id' => 24,
@@ -39,14 +32,36 @@ protected $user;
             ]
         ]);
 
+        $basket = Basket::where('user_id', $user->id)->first();
+        $first_product = Variant::where('product_id', 1)
+            ->where('size_id', 24)
+            ->where('color_id', 1)
+            ->first();
+        $second_product = Variant::where('product_id', 42)
+            ->where('size_id', 24)
+            ->where('color_id', 1)
+            ->first();
+
+
         $response->assertStatus(200);
+        $this->assertDatabaseHas('basket_variant', [
+            'basket_id' => $basket->id,
+            'variant_id' => $first_product->id
+        ]);
+        $this->assertDatabaseHas('basket_variant', [
+            'basket_id' => $basket->id,
+            'variant_id' => $second_product->id,
+            'quantity' => 2
+        ]);
+
 
         $get_basket = $this->actingAs($user)->getJson('api/basket');
 
         $get_basket->assertStatus(200);
     }
 
-    public function test_user_can_see_an_empty_basket() {
+    public function test_user_can_see_an_empty_basket()
+    {
         $user = User::factory()->create();
         $response = $this->actingAs($user)->getJson('api/basket');
 
@@ -54,16 +69,18 @@ protected $user;
 
     }
 
-    public function test_user_can_delete_product() {
+    public function test_user_can_delete_product()
+    {
+        $user = User::factory()->create();
 
-        $response = $this->actingAs($this->user)->postJson('api/basket', [[
+        $response = $this->actingAs($user)->postJson('api/basket', [[
             'product_id' => 1,
             'size_id' => 24,
             'color_id' => 1,
             'quantity' => 1
         ]]);
 
-        $response = $this->actingAs($this->user)->deleteJson('api/basket', [[
+        $response = $this->actingAs($user)->deleteJson('api/basket', [[
             'product_id' => 1,
             'size_id' => 24,
             'color_id' => 1,

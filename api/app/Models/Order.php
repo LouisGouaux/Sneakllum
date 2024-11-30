1df733d132b40f7a34764ae7d9206466f3486074
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2024-11-30 22:39:21 +0100

    Feat [API]: Created relationship between Order and Variant models

--- api/app/Models/Order.php
+++ api/app/Models/Order.php
@@ -13,4 +13,9 @@ class Order extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function variants()
+    {
+        return $this->belongsToMany(Variant::class);
+    }
 }

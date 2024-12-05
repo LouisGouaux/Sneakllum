889a2de51828e2a5c04f1799e89553d312c4b4af
LouisGouaux <72010328+LouisGouaux@users.noreply.github.com>
2024-12-06 00:06:39 +0100

    Test [API]: Removed admin middleware

--- api/routes/api.php
+++ api/routes/api.php
@@ -8,17 +8,18 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('basket', [\App\Http\Controllers\API\BasketController::class, 'store']);
     Route::get('basket', [\App\Http\Controllers\API\BasketController::class, 'show']);
     Route::delete('basket', [\App\Http\Controllers\API\BasketController::class, 'delete_products']);
+    Route::post('products/stock', [\App\Http\Controllers\API\ProductController::class, 'update_stock']);
+    Route::post('products', [\App\Http\Controllers\API\ProductController::class, 'store'])->middleware(\App\Http\Middleware\EnsureUserIsAdmin::class);
+
 
 });
 
 Route::post('register', [\App\Http\Controllers\API\AuthController::class, 'register']);
 Route::post('login', [\App\Http\Controllers\API\AuthController::class, 'login']);
 Route::get('products/search', [\App\Http\Controllers\API\ProductController::class, 'search']);
-Route::post('products/stock', [\App\Http\Controllers\API\ProductController::class, 'update_stock'])->middleware(\App\Http\Middleware\EnsureUserIsAdmin::class);
 Route::get('products/{id}', [\App\Http\Controllers\API\ProductController::class, 'show']);
 Route::get('products/{id}/recommendations', [\App\Http\Controllers\API\ProductController::class, 'recommendation']);
 Route::get('products/{id}/check', [\App\Http\Controllers\API\ProductController::class, 'check_product_stock']);
 Route::get('products', [\App\Http\Controllers\API\ProductController::class, 'index']);
-Route::post('products', [\App\Http\Controllers\API\ProductController::class, 'store'])->middleware(\App\Http\Middleware\EnsureUserIsAdmin::class);
 Route::get('brands', [\App\Http\Controllers\API\FilterController::class, 'brand_index']);
 Route::get('sizes', [\App\Http\Controllers\API\FilterController::class, 'size_index']);
